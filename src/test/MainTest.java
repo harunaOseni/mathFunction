3ea7bbeb52bbfa7746bb95230bc023c7517f4e3a
Haruna Oseni <harunaoseni23@gmail.com>
2022-09-11 12:02:25 -0500

    add: subtraction operation test

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -18,14 +18,19 @@ public class MainTest {
                 fail("mathFunction(1, 2, 3) should return +");
             }
             if (main.mathFunction(2, 2, 4).equals("+*")) {
-                System.out.println("+");
+                System.out.println("+*");
             } else {
                 fail("mathFunction(2, 2, 4) should return +*");
             }
             if (main.mathFunction(3, -3, -9).equals("*")) {
-                System.out.println("-");
+                System.out.println("*");
             } else {
                 fail("mathFunction(3, -3, -9) should return *");
+            }
+            if (main.mathFunction(1, 2, -1).equals("-")) {
+                System.out.println("-");
+            } else {
+                fail("mathFunction(1, 2, -1) should return -");
             }
             if (main.mathFunction(3, 3, 1).equals("/")) {
                 System.out.println("/");
